cfc08b93068f156232ec9205d2e35a952359c25f
Charmink <mry2002@mail.ru>
2020-11-03 19:32:40 +0300

    first

---
+++ stepik_tours/tours/views.py
@@ -0,0 +1,46 @@
+from django.views.generic import TemplateView
+import data
+from random import randrange
+from django.http import HttpResponseNotFound
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('К сожалению не удалось найти указанную страницу :(')
+
+
+class MainView(TemplateView):
+    template_name = 'tours/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainView, self).get_context_data(**kwargs)
+        context['title'] = data.title
+        context['tours'] = [data.tours[randrange(1, 16)] for i in range(6)]
+        context['description'] = data.description
+        context['departures'] = data.departures
+        context['subtitle'] = data.subtitle
+        return context
+
+
+class DepartureView(TemplateView):
+    template_name = 'tours/departure.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DepartureView, self).get_context_data(**kwargs)
+        context['title'] = data.title
+        context['tours'] = [tour for tour in data.tours.values() if tour['departure'] ==
+                            context['departure']]
+        context['departures'] = data.departures
+        context['departure'] = data.departures[context['departure']]
+        return context
+
+
+class TourView(TemplateView):
+    template_name = 'tours/tour.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TourView, self).get_context_data(**kwargs)
+        context['tour'] = data.tours[kwargs['pk']]
+        context['title'] = data.title
+        context['departure'] = data.departures[context['tour']['departure']]
+        context['departures'] = data.departures
+        return context
